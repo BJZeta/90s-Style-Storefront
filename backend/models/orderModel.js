4c7cd894ef98861c0a6edb167a67f5242b4c2d0a
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-12-08 20:04:24 -0800

    adding Order Schema

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -5,34 +5,66 @@ const orderSchema = mongoose.Schema({
     required: true,
     ref: "User",
   },
-  orderedItems: {
-    name: { type: String, required: true },
-    qty: { type: Number, required: true },
-    name: { type: String, required: true },
-    name: { type: String, required: true },
-    //////////////////////////////////////////
-    //////Continue building order Schema//////
-    //////////////////////////////////////////
-  },
-  price: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  inStock: {
-    type: Boolean,
-    required: true,
-    default: true,
-  },
-  description: {
-    type: String,
-  },
-  images: [
+  orderedItems: [
     {
-      type: String,
-      required: true,
+      name: { type: String, required: true },
+      qty: { type: Number, required: true },
+      price: { type: Number, required: true },
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Item",
+      },
     },
   ],
+  shippingAddress: {
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    postalCode: { type: Number, required: true },
+    country: { type: String, required: true },
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  paymentResult: {
+    id: { type: String },
+    status: { type: String },
+    update_time: { type: String },
+    email_address: { type: String },
+  },
+  taxPrice: {
+    type: Number,
+    required: true,
+    default: 0.0,
+  },
+  shippingPrice: {
+    type: Number,
+    required: true,
+    default: 0.0,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+    default: 0.0,
+  },
+  isPaid: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  paidAt: {
+    type: Date,
+  },
+  isDelivered: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  deliveredAt: {
+    type: Date,
+  },
 });
 
 const Order = mongoose.model("Order", orderSchema);
