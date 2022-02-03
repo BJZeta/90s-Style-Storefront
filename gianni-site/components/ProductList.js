e8f93a2ed453249b98eeff9d972363f5b2a5b40f
Brandon Unzueta <unzueta.brandon@gmail.com>
2022-02-02 20:37:39 -0800

    adding tailwindcss to project

---
+++ gianni-site/components/ProductList.js
@@ -0,0 +1,5 @@
+import { getHomePageProducts } from "../lib/shopify";
+
+export default function ProductList({}) {
+  return <div></div>;
+}
--- gianni-site/pages/index.js
+++ gianni-site/pages/index.js
@@ -1,13 +1,12 @@
 import Layout from "../components/Layout";
 import { getHomePageProducts } from "../lib/shopify";
-import styles from "../styles/Home.module.css";
 
 export default function Home({products}) {
   console.log(products)
 
   return (
     <Layout>
-      <div>
+      <div className="text-3xl font-bold underline">
         <h1>Welcome to Gianni's Shop</h1>
         <p>Bitches.</p>
       </div>
